1ae23b30fc16ad8f37ff0da59f204cb632f69947
soroush62 <112301194+soroush62@users.noreply.github.com>
2024-06-19 08:28:16 +0300

    modifying the path

--- Python Files/BarChart.py
+++ Python Files/BarChart.py
@@ -8,7 +8,7 @@ with open('D:/Computer Aplication/WorkPlacement/Projects/shared_variable.txt', '
 lc.set_license(mylicensekey)
 
 # Load the CSV file
-file_path = 'D:/wenprograming23/src/team6/Marine-Seismic-Data-Time-Series-Analysis-with-LightningChart-Python/Data/output.csv'
+file_path = 'D:/Computer Aplication/WorkPlacement/Projects/Project3/namss.F-12-89-HW.mcs.airgun/Data/output.csv'
 df = pd.read_csv(file_path)
 
 # Calculate histogram data
