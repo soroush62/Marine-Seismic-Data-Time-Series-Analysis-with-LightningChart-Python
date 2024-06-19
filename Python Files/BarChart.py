ae24b752af484b4863b4dd541f8b5952f797adc8
soroush62 <112301194+soroush62@users.noreply.github.com>
2024-06-19 08:10:09 +0300

    Moving necessary files to repository

---
+++ Python Files/BarChart.py
@@ -0,0 +1,35 @@
+import lightningchart as lc
+import pandas as pd
+import numpy as np
+
+# Read the license key from a file
+with open('D:/Computer Aplication/WorkPlacement/Projects/shared_variable.txt', 'r') as f:
+    mylicensekey = f.read().strip()
+lc.set_license(mylicensekey)
+
+# Load the CSV file
+file_path = 'D:/wenprograming23/src/team6/Marine-Seismic-Data-Time-Series-Analysis-with-LightningChart-Python/Data/output.csv'
+df = pd.read_csv(file_path)
+
+# Calculate histogram data
+hist, bin_edges = np.histogram(df['trace_value'], bins=50)
+
+# Prepare data for the bar chart
+data = [{'category': f'{(bin_edges[i] + bin_edges[i + 1]) / 2:.2f}', 'value': int(hist[i])} for i in range(len(hist))]
+
+# Create a Bar Chart
+chart = lc.BarChart(
+    vertical=True,
+    theme=lc.Themes.Dark,
+    title='Distribution of amplitude values'
+)
+
+# Set the data for the bar chart
+chart.set_data(data)
+
+# Show the chart
+chart.open()
+
+# Add axis titles using text boxes
+x_axis_title = chart.add_textbox(text='Amplitude', x=50, y=95)
+y_axis_title = chart.add_textbox(text='Frequency', x=5, y=50)
