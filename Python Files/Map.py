f1a890a1a79f7fb6f5bb90a1496e68f221cf5483
soroush62 <112301194+soroush62@users.noreply.github.com>
2024-06-25 09:07:53 +0300

    editing map

--- Python Files/Map.py
+++ Python Files/Map.py
@@ -1,25 +1,19 @@
 import lightningchart as lc
 
-# Read the license key from a file
 with open('D:/Computer Aplication/WorkPlacement/Projects/shared_variable.txt', 'r') as f:
     mylicensekey = f.read().strip()
 lc.set_license(mylicensekey)
 
-# Define data for Hawaii
 region_data = [
-    {'ISO_A3': "USA-HI", 'value': 1}  # Dummy data to ensure map initializes properly
+    {'ISO_A3': "USA", 'value': 1}  
 ]
 
-# Initialize the map chart
 chart = lc.MapChart(map_type='USA', theme=lc.Themes.Dark, title='Seismic Data Points in Hawaii')
 
-# Invalidate region values with the dummy data for Hawaii
 chart.invalidate_region_values(region_data)
 
-# Customize the chart
 chart.set_highlight_on_hover(enabled=True)
 
-# Set palette colors to visualize the data without making everything black
 chart.set_palette_colors(
     steps=[
         {'value': 0, 'color': lc.Color('#fcba03')},
@@ -29,5 +23,4 @@ chart.set_palette_colors(
     percentage_values=False
 )
 
-# Open the chart
-chart.open()
+chart.open(live=True)
