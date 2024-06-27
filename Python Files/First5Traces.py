2edec8988e34c295f612ec4e6c46ca606a2ab7cb
soroush62 <112301194+soroush62@users.noreply.github.com>
2024-06-27 11:20:55 +0300

    removing unnecessary scripts to clean files

--- Python Files/First5Traces.py
+++ Python Files/First5Traces.py
@@ -5,25 +5,27 @@ with open('D:/Computer Aplication/WorkPlacement/Projects/shared_variable.txt', '
     mylicensekey = f.read().strip()
 lc.set_license(mylicensekey)
 
-df = pd.read_csv('D:/Computer Aplication/WorkPlacement/Projects/Project3/namss.F-12-89-HW.mcs.airgun/Data/output.csv')
+file_path = 'D:/Computer Aplication/WorkPlacement/Projects/Project3/namss.F-12-89-HW.mcs.airgun/Data/output.csv'
+df = pd.read_csv(file_path)
 
 df['time_ms'] = df['sample_number'] * df['sample_interval_in_ms_for_this_trace'] / 1000.0
 
+trace_max_amplitudes = df.groupby('trace_sequence_number_within_line')['trace_value'].max()
+top_traces = trace_max_amplitudes.nlargest(5).index
+
 chart = lc.ChartXY(
     theme=lc.Themes.Black,
-    title='Time-Series Analysis of Seismic Data'
+    title='Top 5 Traces with Largest Amplitude Values'
 )
 
 chart.get_default_y_axis().dispose()
 
 legend = chart.add_legend()
 
-unique_traces = df['trace_sequence_number_within_line'].unique()
-
-for i, trace in enumerate(unique_traces[:5]):
+for i, trace in enumerate(top_traces):
     trace_data = df[df['trace_sequence_number_within_line'] == trace]
     axis_y = chart.add_y_axis(stack_index=i)
-    axis_y.set_margins(15 if i > 0 else 0, 15 if i < 3 else 0)  
+    axis_y.set_margins(15 if i > 0 else 0, 15 if i < 4 else 0)  
     axis_y.set_title(title=f'Trace {trace}')
     series = chart.add_line_series(y_axis=axis_y, data_pattern='ProgressiveX')
     series.add(trace_data['time_ms'].tolist(), trace_data['trace_value'].tolist())
--- Python Files/TimeSeries.py
+++ Python Files/TimeSeries.py
@@ -1,31 +1,3 @@
-# import matplotlib.pyplot as plt
-# import pandas as pd
-
-# # Load the CSV file
-# df = pd.read_csv('D:/Computer Aplication/WorkPlacement/Projects/Project3/namss.F-12-89-HW.mcs.airgun/Data/output.csv')
-
-# # Convert sample number to time (assuming the sample interval is in microseconds)
-# df['time_ms'] = df['sample_number'] * df['sample_interval_in_ms_for_this_trace'] / 1000.0
-
-# # Plot the time-series data for the first few traces
-# fig, ax = plt.subplots(figsize=(15, 6))
-
-# # Plotting traces: assuming you want to plot the first few traces
-# unique_traces = df['trace_sequence_number_within_line'].unique()
-# for trace in unique_traces[:1]:  # Plot first 5 traces for example
-#     trace_data = df[df['trace_sequence_number_within_line'] == trace]
-#     ax.plot(trace_data['time_ms'], trace_data['trace_value'], label=f'Trace {trace}')
-
-# # Set labels and title
-# ax.set_xlabel('Time (ms)')
-# ax.set_ylabel('Amplitude')
-# ax.set_title('Time-Series Analysis of Seismic Data')
-# ax.legend()
-
-# # Show the plot
-# plt.show()
-
-
 import lightningchart as lc
 import pandas as pd
 
